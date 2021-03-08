513bf6d29bcbece7fbfcf16862b34e8d6545a8cd
Solidshots <seanbrent5@gmail.com>
2021-03-08 08:33:19 -0500

    Add task button functionality

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var buttonEl = document.querySelector("#save-task");
+var taskListEl = document.querySelector("#tasks-to-do");
+
+function createTaskHandler(){
+    var taskItemEl = document.createElement("li");
+    taskItemEl.className = "task-item";
+    taskItemEl.textContent = "Another task added!";
+    taskListEl.appendChild(taskItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
