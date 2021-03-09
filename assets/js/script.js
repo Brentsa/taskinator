537da4de09d1cca5db237586e2409f7f71a873b1
Solidshots <seanbrent5@gmail.com>
2021-03-09 08:37:26 -0500

    functioning add task button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
 var formEl = document.querySelector("#task-form");
-var taskListEl = document.querySelector("#tasks-to-do");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 function createTaskHandler(event){
     event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
-    var taskItemEl = document.createElement("li");
-    taskItemEl.className = "task-item";
-    taskItemEl.textContent = "New Task";
-    taskListEl.appendChild(taskItemEl);
-    
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class ='task-name'>" + taskNameInput + "</h3><span class = 'task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+    tasksToDoEl.appendChild(listItemEl);    
 }
 
 formEl.addEventListener("submit", createTaskHandler);
