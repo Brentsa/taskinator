80fe441e89085c9b01255da06f7dfcb9e30f3866
Solidshots <seanbrent5@gmail.com>
2021-03-09 07:51:36 -0500

    changed some form code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var taskListEl = document.querySelector("#tasks-to-do");
 
-function createTaskHandler(){
+function createTaskHandler(event){
+    event.preventDefault();
+    
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
+    taskItemEl.textContent = "New Task";
     taskListEl.appendChild(taskItemEl);
+    
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
